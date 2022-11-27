278ce6fd41ac7a5b5ae022233bd1e9c266e987e7
prefie <prefie@bk.ru>
2022-11-27 15:58:55 +0500

    add category + new controller

---
+++ app/backend/MemuaroApp/Memuaro.WebApi/Dtos/Question/GetGlobalQuestionsRequestDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Memuaro.WebApi.Dtos.Question;
+
+public class GetGlobalQuestionsRequestDto
+{
+    public Guid? UserId { get; set; }
+    public Guid[]? Categories { get; set; }
+}
---
+++ app/backend/MemuaroApp/Memuaro.WebApi/Dtos/Question/GlobalQuestionDto.cs
@@ -0,0 +1,15 @@
+﻿using Memuaro.Persistance.Entities;
+
+namespace Memuaro.WebApi.Dtos.Question;
+
+public class GlobalQuestionDto
+{
+    public string? Title { get; set; }
+    public Guid CategoryId { get; set; }
+
+    public GlobalQuestionDto(GlobalQuestion globalQuestion)
+    {
+        Title = globalQuestion.Title;
+        CategoryId = globalQuestion.CategoryId;
+    }
+}
